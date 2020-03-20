5f9fe95869b7964e5dde96b7092ad09194805079
AntiD2ta <mtpotro41@gmail.com>
2020-03-20 13:44:54 -0400

    Add main file. Add params for program execution

---
+++ simulation.py
@@ -0,0 +1,15 @@
+
+def main(args):
+    pass
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser(description='People evolution simulator')
+    parser.add_argument('-m', '--males', type=int, default=800000, help='number of initial male people')
+    parser.add_argument('-f', '--female', type=int, default=1000000, help='number of initial female people')
+    parser.add_argument('-p', '--period', type=int, default=4800, help='number of weeks of the entire simulation period')
+
+    args = parser.parse_args()
+
+    main(args)
