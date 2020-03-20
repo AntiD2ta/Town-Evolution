954609c7b6c1b5e2d7d4c34cf4993539dcbb8b7e
AntiD2ta <mtpotro41@gmail.com>
2020-03-20 16:44:37 -0400

    Create class Scope
    Scope is for keep and change states of the simulation process

--- simulation.py
+++ simulation.py
@@ -1,11 +1,15 @@
 import utils
 
+log = utils.LoggerFactory(name="Simulator")
+
+
 class Person:
 
     def __init__(self, sex, age):
         self.sex = sex
         self.age = age
         self.childrenNumber = utils.generateChildrenNumber()
+        self.inmunity = 0
 
         if sex == 'f':
             self.pregnant = False
@@ -13,6 +17,46 @@ class Person:
     def ageInYears(self):
         return self.sex // 48
 
+
+class Scope:
+
+    def __init__(self, m, f, period, kind, lamb):
+        self.turns = utils.generateTimes(kind, lamb)
+        self.top = m + f
+        self.people = dict()
+
+        for _ in range(m):
+            self.people[len(self.people) + 1] = Person("male", utils.randint(0, 100))
+        for _ in range(f):
+            self.people[len(self.people) + 1] = Person("female", utils.randint(0, 100))
+
+        self.actual_time = 0
+        self.period = period
+        self.events = set()
+        self.inmunities = set()
+
+    def simulate(self):
+        #generate deaths events
+        for k, p in self.people.items():
+            if not p.inmunity:
+                result, t = utils.generateDeath(p.sex, p.age, self.actual_time)
+                if result:
+                    self.events.add((t, "death"))
+                else:
+                    p.inmunity = t
+                    self.inmunities.add(k)
+
+        #generate breakups
+
+        #check events
+
+        #generate couple wishes
+
+        #set couples
+
+        #generate pregnats
+
+        #update date
 
 def main(args):
     pass
